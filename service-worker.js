4db1b672b7f5094c11fdd4c564a0f082afeb892e
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2025-09-09 00:01:58 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-// let CACHE_NAME = "currency-calculator-cache-v1";
-// let urlsToCache = [
-//   "./",
-//   "./index.html",
-//   "./curr con.css",
-//   "./curr con.js",
-//   "./currency cal icon.ico",
-// ];
+let CACHE_NAME = "currency-calculator-cache-v1";
+let urlsToCache = [
+  "./",
+  "./index.html",
+  "./curr con.css",
+  "./curr con.js",
+  "./currency cal icon.ico",
+];
 
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-//   );
-// });
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
 
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     caches.match(e.request).then((cachedResponse) => {
-//       return cachedResponse || fetch(e.request);
-//     })
-//   );
-// });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
